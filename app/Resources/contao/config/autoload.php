73bea016b39fd84aefc9720d5fc2777966ae0634
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2016-08-08 19:01:58 +0200

    Removed the redundant AppBundle

---
+++ app/Resources/contao/config/autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(
+    [
+        'ce_accordionSingle' => 'app/Resources/contao/templates',
+        'ce_headline'        => 'app/Resources/contao/templates',
+        'ce_html_teilnehmer' => 'app/Resources/contao/templates',
+        'fe_contao_camp'     => 'app/Resources/contao/templates',
+        'mod_article'        => 'app/Resources/contao/templates',
+    ]
+);
