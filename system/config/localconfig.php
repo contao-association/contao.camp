340848b638e885b076ce0f2bfd885ab3a066e697
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-08-24 14:13:16 +0200

    Disable poor man's cron, we're running cron jobs on the server

--- system/config/localconfig.php
+++ system/config/localconfig.php
@@ -2,5 +2,6 @@
 
 ### INSTALL SCRIPT START ###
 $GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
-$GLOBALS['TL_CONFIG']['adminEmail'] = 'association@contao.org';
+$GLOBALS['TL_CONFIG']['adminEmail']      = 'association@contao.org';
+$GLOBALS['TL_CONFIG']['disableCron']     = true;
 ### INSTALL SCRIPT STOP ###
