881869503b3bd87acefd4507e057126c0f89830b
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-11-15 11:47:57 +0100

    Disable search index

--- system/config/localconfig.php
+++ system/config/localconfig.php
@@ -4,4 +4,7 @@
 $GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
 $GLOBALS['TL_CONFIG']['adminEmail']      = 'association@contao.org';
 $GLOBALS['TL_CONFIG']['disableCron']     = true;
+$GLOBALS['TL_CONFIG']['enableSearch']    = false;
+$GLOBALS['TL_CONFIG']['datimFormat']     = 'd.m.Y H:i';
+$GLOBALS['TL_CONFIG']['dateFormat']      = 'd.m.Y';
 ### INSTALL SCRIPT STOP ###
